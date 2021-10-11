b4e712632af527b8a679cac1fa14f372643bbf04
Jayden Connally <jayden.connally01@gmail.com>
2021-10-11 12:27:04 -0400

    Update RoomSpawning.cs

--- ProceduralAssignment/Assets/Scripts/RoomSpawning.cs
+++ ProceduralAssignment/Assets/Scripts/RoomSpawning.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RoomSpawning : MonoBehaviour
 {
+    public int DoorDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,24 @@ public class RoomSpawning : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (DoorDirection==1)
+        {
+            //Spawn Door with right Exit
+        }
+
+        if (DoorDirection == 2)
+        {
+            //Spawn Room With Left Exit
+        }
+
+        if (DoorDirection == 3)
+        {
+            //Spawn Room with Up exit
+        }
+
+        if (DoorDirection==4)
+        {
+            //Spawn Room with Down Exit
+        }
     }
 }
