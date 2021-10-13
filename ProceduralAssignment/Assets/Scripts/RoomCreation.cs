e1e0c3f5da8dd8dae4da0e203dd39ca179255fda
Jayden Connally <jayden.connally01@gmail.com>
2021-10-13 12:46:33 -0400

    prototype.
    finished all the rooms,  code is done.  Error regarding."NullReferenceException: Object reference not set to an instance of an object
    RoomSpawning.Spawningrooms () (at Assets/Scripts/RoomSpawning.cs:35)
    RoomSpawning.Start () (at Assets/Scripts/RoomSpawning.cs:18)"
    Ask nick how to fix it.

--- ProceduralAssignment/Assets/Scripts/RoomCreation.cs
+++ ProceduralAssignment/Assets/Scripts/RoomCreation.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class RoomCreation : MonoBehaviour
 {
     [Header("Room Types")]
-    public GameObject[] RightExitRooms; //1
-    public GameObject[] LeftExitRooms; //2
-    public GameObject[] TopExitRooms; //3
-    public GameObject[] BottomExitRooms; //4
+    public GameObject[] RightExitRooms; //1:This means use Right Exitrooms for Doors that are to the left.
+    public GameObject[] LeftExitRooms; //2: This means use Left exitrooms for Doors that are to the Right.
+    public GameObject[] TopExitRooms; //3:This means use Top Exit Rooms for Door that are at the bottom.
+    public GameObject[] BottomExitRooms; //4:This means use the Bottom Exit Doors for rooms that are at the top.
 
     // Start is called before the first frame update
     void Start()
--- ProceduralAssignment/Assets/Scripts/RoomSpawning.cs
+++ ProceduralAssignment/Assets/Scripts/RoomSpawning.cs
@@ -6,36 +6,71 @@ public class RoomSpawning : MonoBehaviour
 {
     public int DoorDirection;
 
+    [Header("Room Spawning Stuff")]
     private RoomCreation RoomTemplate;
+    private int randRoom;
+    public bool HasBeenSpawned = false;
    
     
     // Start is called before the first frame update
     void Start()
     {
         RoomTemplate = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomCreation>();
+        Spawningrooms();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (DoorDirection==1)
-        {
-            //Spawn Door with right Exit
-        }
 
-        if (DoorDirection == 2)
-        {
-            //Spawn Room With Left Exit
-        }
 
-        if (DoorDirection == 3)
-        {
-            //Spawn Room with Up exit
-        }
+    }
 
-        if (DoorDirection==4)
+    void Spawningrooms()
+    {
+        if (HasBeenSpawned == false)
         {
-            //Spawn Room with Down Exit
+            if (DoorDirection == 1) //For a right door
+            {
+                //Spawn Door with Left Entrance an exit.
+                randRoom = Random.Range(0, RoomTemplate.LeftExitRooms.Length);
+                Instantiate(RoomTemplate.LeftExitRooms[randRoom], transform.position, transform.rotation);
+            }
+
+            if (DoorDirection == 2)//For a left Door.
+            {
+                //Spawn Room With Right Exit Door
+                randRoom = Random.Range(0, RoomTemplate.RightExitRooms.Length);
+                Instantiate(RoomTemplate.RightExitRooms[randRoom], transform.position, transform.rotation);
+            }
+
+            if (DoorDirection == 3)//For Top Door.
+            {
+                //Spawn Room with Down exit.
+                randRoom = Random.Range(0, RoomTemplate.BottomExitRooms.Length);
+                Instantiate(RoomTemplate.BottomExitRooms[randRoom], transform.position, transform.rotation);
+            }
+
+            if (DoorDirection == 4)//For Bottom door
+            {
+                //Spawn Room with an up Exit.
+                randRoom = Random.Range(0, RoomTemplate.TopExitRooms.Length);
+                Instantiate(RoomTemplate.TopExitRooms[randRoom], transform.position, transform.rotation);
+            }
+            HasBeenSpawned = true;
         }
+    }
+
+    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // If the spawnpoint tags collide with eachother HasBeenSpawned is true.
+        if (other.CompareTag("SpawnPoint"))
+        {
+            Destroy(gameObject);
+        }
+        
+
     }
 }
