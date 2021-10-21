188b605b8b4f5c4045fa5064aff402dfcd298cd8
Jayden Connally <jayden.connally01@gmail.com>
2021-10-20 21:55:26 -0400

    Finally Fixed error.  Now time to fix generation

--- ProceduralAssignment/Assets/Scripts/RoomSpawning.cs
+++ ProceduralAssignment/Assets/Scripts/RoomSpawning.cs
@@ -8,59 +8,104 @@ public class RoomSpawning : MonoBehaviour
 
     [Header("Room Spawning Stuff")]
     private RoomCreation RoomTemplate;
-    private int randRoom;
+    private int RandRoom;
     public bool HasBeenSpawned = false;
    
     
     // Start is called before the first frame update
     void Start()
     {
-        RoomTemplate = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomCreation>();
-        Spawningrooms();
+        RoomTemplate = GetComponent<RoomCreation>();
+        RoomSpawn();
+        //Spawningrooms();
+        
+        
     }
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+
+
+    //}
+
+    //void Spawningrooms()
+    //{
+    //    if (HasBeenSpawned == false)
+    //    {
+    //        if (DoorDirection == 1) //For a right door
+    //        {
+    //            //Spawn Door with Left Entrance an exit.
+    //            RandRoom = Random.Range(0, RoomTemplate.LeftExitRooms.Length);
+    //            Instantiate(RoomTemplate.LeftExitRooms[RandRoom], transform.position, transform.rotation);
+    //        }
+
+    //        if (DoorDirection == 2)//For a left Door.
+    //        {
+    //            //Spawn Room With Right Exit Door
+    //            RandRoom = Random.Range(0, RoomTemplate.RightExitRooms.Length);
+    //            Instantiate(RoomTemplate.RightExitRooms[RandRoom], transform.position, transform.rotation);
+    //        }
+
+    //        if (DoorDirection == 3)//For Top Door.
+    //        {
+    //            //Spawn Room with Down exit.
+    //            RandRoom = Random.Range(0, RoomTemplate.BottomExitRooms.Length);
+    //            Instantiate(RoomTemplate.BottomExitRooms[RandRoom], transform.position, transform.rotation);
+    //        }
+
+    //        if (DoorDirection == 4)//For Bottom door
+    //        {
+    //            //Spawn Room with an up Exit.
+    //            RandRoom = Random.Range(0, RoomTemplate.TopExitRooms.Length);
+    //            Instantiate(RoomTemplate.TopExitRooms[RandRoom], transform.position, transform.rotation);
+    //        }
+    //        HasBeenSpawned = true;
+    //        print(RoomTemplate);
+    //    }
+    //}
+
+    
+    void RoomSpawn()
     {
-
-
-    }
-
-    void Spawningrooms()
-    {
-        if (HasBeenSpawned == false)
+        switch (DoorDirection)
         {
-            if (DoorDirection == 1) //For a right door
-            {
-                //Spawn Door with Left Entrance an exit.
-                randRoom = Random.Range(0, RoomTemplate.LeftExitRooms.Length);
-                Instantiate(RoomTemplate.LeftExitRooms[randRoom], transform.position, transform.rotation);
-            }
+            case 1:
+                if (RoomTemplate.LeftExitRooms.Length > 0)
+                {
+                    RandRoom = Random.Range(0, RoomTemplate.LeftExitRooms.Length - 1);
+                    Instantiate(RoomTemplate.LeftExitRooms[RandRoom], transform.position, transform.rotation);
+                }
+               break;
 
-            if (DoorDirection == 2)//For a left Door.
-            {
-                //Spawn Room With Right Exit Door
-                randRoom = Random.Range(0, RoomTemplate.RightExitRooms.Length);
-                Instantiate(RoomTemplate.RightExitRooms[randRoom], transform.position, transform.rotation);
-            }
+            case 2:
+                if (RoomTemplate.RightExitRooms.Length > 0)
+                {
+                    RandRoom = Random.Range(0, RoomTemplate.RightExitRooms.Length - 1);
+                    Instantiate(RoomTemplate.RightExitRooms[RandRoom], transform.position, transform.rotation);
+                }
+                break;
 
-            if (DoorDirection == 3)//For Top Door.
-            {
+            case 3:
                 //Spawn Room with Down exit.
-                randRoom = Random.Range(0, RoomTemplate.BottomExitRooms.Length);
-                Instantiate(RoomTemplate.BottomExitRooms[randRoom], transform.position, transform.rotation);
-            }
+                if (RoomTemplate.BottomExitRooms.Length>0)
+                {
+                    RandRoom = Random.Range(0, RoomTemplate.BottomExitRooms.Length - 1);
+                    Instantiate(RoomTemplate.BottomExitRooms[RandRoom], transform.position, transform.rotation);
+                }
+               break;
 
-            if (DoorDirection == 4)//For Bottom door
-            {
+            case 4:
                 //Spawn Room with an up Exit.
-                randRoom = Random.Range(0, RoomTemplate.TopExitRooms.Length);
-                Instantiate(RoomTemplate.TopExitRooms[randRoom], transform.position, transform.rotation);
-            }
-            HasBeenSpawned = true;
+                if (RoomTemplate.TopExitRooms.Length > 0)
+                {
+                    RandRoom = Random.Range(0, RoomTemplate.TopExitRooms.Length - 1);
+                    Instantiate(RoomTemplate.TopExitRooms[RandRoom], transform.position, transform.rotation);
+                }
+                break;
         }
+        HasBeenSpawned = true;
     }
-
     
     
     private void OnTriggerEnter2D(Collider2D other)
